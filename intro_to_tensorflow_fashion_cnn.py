e6189bcebeb895a48551c2f5334cfc24ff3a84dd
Scott <scott@linfords.com>
2022-07-10 07:22:02 -0700

    Code cleanup only

--- intro_to_tensorflow_fashion_cnn.py
+++ intro_to_tensorflow_fashion_cnn.py
@@ -1,37 +1,15 @@
 import time
-
 import tensorflow as tf
-# Import TensorFlow Datasets
 import tensorflow_datasets as tfds
-
-tfds.disable_progress_bar()
-
-# Helper libraries
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
-
-logger = tf.get_logger()
-logger.setLevel(logging.ERROR)
-
 start_time = time.time()
+
+tfds.disable_progress_bar()
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
-
-# Load the fashion data set
-dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
-train_dataset, test_dataset = dataset['train'], dataset['test']
-
-# Class names aren't part of the data itself.
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-# Explore the data
-num_train_examples = metadata.splits['train'].num_examples
-num_test_examples = metadata.splits['test'].num_examples
-print("Number of training examples: {}".format(num_train_examples))
-print("Number of test examples:     {}".format(num_test_examples))
 
 
 def normalize(images, labels):
@@ -40,18 +18,30 @@ def normalize(images, labels):
     return images, labels
 
 
-# The map function applies the normalize function to each element in the train
-# and test datasets
+# Load the fashion data set
+dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+# Class names
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Explore the data
+num_train_examples = metadata.splits['train'].num_examples
+num_test_examples = metadata.splits['test'].num_examples
+print(f'Number of training examples: {num_train_examples}')
+print(f'Number of test examples:     {num_train_examples}')
+
+# The map function applies the normalize function to each element in the train and test datasets
 train_dataset = train_dataset.map(normalize)
 test_dataset = test_dataset.map(normalize)
 
-# Load dataset into memory
+# Load dataset into memory for faster processing
 train_dataset = train_dataset.cache()
 test_dataset = test_dataset.cache()
 
 # Take a single image, and remove the color dimension by reshaping
 for image, label in test_dataset.take(1):
-    break
+    pass
 image = image.numpy().reshape((28, 28))
 
 # Plot the image - voila a piece of fashion clothing
