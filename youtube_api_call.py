fe8a6f42d507d15de043f90c07cf4cdd9c814ce7
Scott <scott@linfords.com>
2022-07-21 00:37:22 -0700

    Removed 'snippet' from part parameter

--- youtube_api_call.py
+++ youtube_api_call.py
@@ -6,7 +6,7 @@ api_key = os.environ.get('api_key')
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 request = youtube.channels().list(
-    part='contentDetails',
+    part='contentDetails, snippet',
     forUsername='ScottLinford'
 )
 
